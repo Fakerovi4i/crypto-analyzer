35e97cf43a9ebec554d4a3adffedf578efb90e65
Vito <fakerovi4i@gmail.com>
2026-08-26 18:59:47 +0300

    fix: fix code review comments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from time import sleep
 import functools
 import json
+from typing import Any
 
 from rich.console import Console
 from rich.table import Table
@@ -29,17 +30,18 @@ def retry(max_attempts: int, delay: int):
     return decorator
 
 @retry(3, 2)
-def top_50_coin() -> list[dict]:
-    response = requests.get(API_URL)
-    response.raise_for_status()
-    return response.json()
+def top_50_coin() -> list[dict] | Any:
+    with console.status("[green] Загрузка...[/]"):
+        response = requests.get(API_URL)
+        response.raise_for_status()
+        return response.json()
 
 def top_3_growth_coin_change(coins: list[dict]) -> list[dict]:
-    sorted_coins = sorted(coins, key=lambda coin: coin["price_change_percentage_24h"], reverse=True)
+    sorted_coins = sorted(coins, key=lambda coin: coin.get("price_change_percentage_24h", 0), reverse=True)
     return sorted_coins[:3]
 
 def top_3_fall_coin_change(coins: list[dict]) -> list[dict]:
-    sorted_coins = sorted(coins, key=lambda coin: coin["price_change_percentage_24h"], reverse=False)
+    sorted_coins = sorted(coins, key=lambda coin: coin.get("price_change_percentage_24h", 0), reverse=False)
     return sorted_coins[:3]
 
 def top_1_total_volume(coins: list[dict]) -> dict:
@@ -50,64 +52,86 @@ def capitalize_coins(coins: list[dict]) -> float:
 
 def show_table(growth: list[dict], fall: list[dict]):
     table = Table(title="Crypto Coins")
+    keys_coins = (
+        'id', 'name', 'symbol', 'price_change_percentage_24h', 'total_volume'
+    )
 
-    with console.status("[green] Загрузка...[/]"):
-        sleep(1)
-        keys_coins = (
-            'id', 'name', 'symbol', 'price_change_percentage_24h', 'total_volume'
-        )
+    for k in keys_coins:
+        table.add_column(k)
 
-        for k in keys_coins:
-            table.add_column(k)
+    for g in growth:
+        table.add_row(*(str(g.get(k)) for k in keys_coins), style="green")
 
-        for g in growth:
-            table.add_row(*(str(g.get(k)) for k in keys_coins), style="green")
+    for f in fall:
+        table.add_row(*(str(f.get(k)) for k in keys_coins), style="red")
 
-        for f in fall:
-            table.add_row(*(str(f.get(k)) for k in keys_coins), style="red")
+    console.print(table)
 
-        console.print(table)
+def to_entry(coin: dict, mapping: dict) -> dict:
+    return {k: coin.get(v, None) for k, v in mapping.items()}
 
-def make_dict(coins: list[dict]):
-    total_market_cap = capitalize_coins(coins)
-    top_3 = top_3_growth_coin_change(coins)
-    lose_3 = top_3_fall_coin_change(coins)
-    high_volume = top_1_total_volume(coins)
+
+def make_dict(
+        top_3: list[dict],
+        lose_3: list[dict],
+        high_volume: dict,
+        total_market_cap: float,
+        len_coins: int
+) -> dict:
     result_dict = {
         "generated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "total_coins_analyzed": len(coins),
+        "total_coins_analyzed": len_coins,
         "total_market_cap_usd": total_market_cap,
         "top_gainers": [],
         "top_losers": [],
         "highest_volume": {}
     }
 
-    keys = ["name", "symbol", "change_24h"]
-    keys_values = ["name", "symbol", "price_change_percentage_24h"]
+    mapping_keys = {
+        "name": "name",
+        "symbol": "symbol",
+        "change_24h": "price_change_percentage_24h"
+    }
 
-    for i in range(3):
-        result_dict["top_gainers"].append({keys[j]: top_3[i][keys_values[j]] for j in range(3)})
+    result_dict["top_gainers"] = [to_entry(coin, mapping_keys) for coin in top_3]
+    result_dict["top_losers"] = [to_entry(coin, mapping_keys) for coin in lose_3]
 
-    for i in range(3):
-        result_dict["top_losers"].append({keys[j]: lose_3[i][keys_values[j]] for j in range(3)})
+    mapping_keys = {
+        "name": "name",
+        "symbol": "symbol",
+        "volume_usd": "total_volume"
+    }
 
-    keys = ["name", "symbol", "volume_usd"]
-    keys_values = ["name", "symbol", "total_volume"]
-    result_dict["highest_volume"] = {keys[i]: high_volume[keys_values[i]] for i in range(3)}
+    result_dict["highest_volume"] = to_entry(high_volume, mapping_keys)
 
     return result_dict
 
-def write_to_file(coins: list[dict]):
-    data = make_dict(coins)
-    with open("crypto_report.json", "w") as file:
+def write_to_file(data: dict) -> None:
+    with open("crypto_report.json", "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 def main():
     top_coins = top_50_coin()
+
+    if not top_coins:
+        console.print("Не удалось получить данные о криптовалютах.")
+        return
+
     growth_coins = top_3_growth_coin_change(top_coins)
     fall_coins = top_3_fall_coin_change(top_coins)
+    high_volume = top_1_total_volume(top_coins)
+    total_market_cap = capitalize_coins(top_coins)
+    len_coins = len(top_coins)
+    data = make_dict(
+        growth_coins,
+        fall_coins,
+        high_volume,
+        total_market_cap,
+        len_coins
+    )
+
     show_table(growth=growth_coins, fall=fall_coins)
-    write_to_file(top_coins)
+    write_to_file(data)
 
 
 if __name__ == "__main__":
