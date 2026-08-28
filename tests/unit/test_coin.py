180a82b2516777be6e509b974f7195877fb17213
Vito <fakerovi4i@gmail.com>
2026-08-28 17:18:01 +0300

    test: add test_coin_validate_failed

--- tests/unit/test_coin.py
+++ tests/unit/test_coin.py
@@ -1,11 +1,8 @@
 import pytest
-
 from main import Coin
 
-
-
-
-def test_coin_create():
+def test_coin_create_successful():
+    """Тут для наглядности не использую фикстуру"""
     coin = Coin(
         id="bitcoin",
         name="Bitcoin",
@@ -23,7 +20,17 @@ def test_coin_create():
     assert coin.market_cap == 50000000.0
 
 
-
-
-
-
+@pytest.mark.parametrize(
+    "params", [
+        ({"id": 123}),
+        ({"name": ""}),
+        ({"symbol": 12}),
+        ({"price_change_percentage_24h": []}),
+        ({"total_volume": -1}),
+        ({"market_cap": -1}),
+        ({"market_cap": ""}),
+    ]
+)
+def test_coin_validate_failed(make_coin_fixture, params):
+    with pytest.raises(ValueError):
+        make_coin_fixture(**params)
